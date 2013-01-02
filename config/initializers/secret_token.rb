aac9d57bf898be759e7d7f187bfd74d490813459
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2013-01-02 01:39:08 -0300

    [Initial Commit]
      - Rails setup.
      - Rspec alone.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommentsDashboard::Application.config.secret_token = '0e54e966cebdf7e1e0ac07867d24f689f931dafacfee71c057e0e4cae0eb2413dcdff807eaa2ac23242cd51e57924f5250deaf192a23fce43d5824fc01b0e641'
