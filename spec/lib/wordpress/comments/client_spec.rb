7f8c2515fc96465d0c90a9a7860dd257604751af
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-01-04 18:12:51 -0300

    Capitulo 5

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -5,14 +5,32 @@ module Wordpress
       def initialize url
         @url = url
       end
+
+      def parse xml
+        
+      end
     end
   end
 end
 
 describe Wordpress::Comments::Client do
-  it "stores a URL" do
-    client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
-    expect(client.url).to eq 'http://mashable.com/comments/feed'
+  describe "#initialize" do
+    it "stores a URL" do
+      client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
+      expect(client.url).to eq 'http://mashable.com/comments/feed'
+    end
   end
-  
+
+  describe "#parse" do
+    let(:xml) {File.read(File.join('spec', 'fixtures', 'feed.xml'))}
+    it "extract the link" do
+      client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
+      comments = client.parse xml 
+      comment = comments.first
+      link = 'http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503'
+      expect(comment.link).to eq link
+    end
+  end
+
 end
+
